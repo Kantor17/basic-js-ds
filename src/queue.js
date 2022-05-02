8b3deb1fa200fdb79a10325f9796c19865d5214d
Maksym Kantor <max.kantor72@gmail.com>
2022-05-02 19:57:57 +0300

    queue solution

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
+  constructor() {
+    this.list = null;
+  }
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.list;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    if(this.list) {
+      let currentNode = this.list;
+      while(currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = new ListNode(value);
+    }else {
+      this.list = new ListNode(value);
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const top = this.list.value;
+    this.list = this.list.next;
+    return top;
   }
 }
 
